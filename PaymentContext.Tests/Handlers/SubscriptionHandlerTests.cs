602b82c5f11b5ce648c5c573abf299143737fb5e
BoltzBit <mateus_desktop@outlook.com>
2020-08-14 10:13:21 -0300

    Finalizando

---
+++ PaymentContext.Tests/Handlers/SubscriptionHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Commands;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Handlers;
+using PaymentContext.Tests.Mocks;
+
+namespace PaymentContext.Tests.Handlers
+{
+    public class SubscriptionHandlerTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorWhenDocumentExists()
+        {
+            var handler = new SubscriptionHandler(new FakeStudentRepository(), new FakeEmailService());
+            var command = new CreateBoletoSubscriptionCommand();
+
+            command.FirstName = "Bruce";
+            command.LastName = "Wayne";
+            command.Document = "99999999999";
+            command.Email = "hello@balta.io";
+            command.BarCode = "123456789";
+            command.BoletoNumber = "123465987";
+            command.PaymentNumber = "123121";
+            command.PaidDate = DateTime.Now;
+            command.ExpireDate = DateTime.Now.AddMonths(1);
+            command.Total = 60;
+            command.TotalPaid = 60;
+            command.Payer = "Wayne Corp";
+            command.PayerDocument = "12345678911";
+            command.PayerEmail = "batman@dc.com";
+            command.PayerDocumentType = EDocumentType.CPF;
+            command.Street = "asdas";
+            command.Number = "36";
+            command.Neighborhood = "asasas";
+            command.City = "asasas";
+            command.State = "asdpçap";
+            command.Country = "asoihsc";
+            command.ZipCode = "12345678";
+
+            handler.Handle(command);
+            Assert.AreEqual(false, handler.Invalid);
+        }
+    }
+}
